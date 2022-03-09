e49f9f7c6af89055f08fb309cf77e96e1a6a9434
lythanhhai <you@example.com>
2022-03-09 17:53:03 +0700

    :tada: test redux with a simple todo app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Todo from './Component/Todo';
+import { useSelector, useDispatch } from 'react-redux';
 
 function App() {
   return (
---
+++ src/Redux_Todo/store.js
@@ -0,0 +1,8 @@
+import { createStore, combineReducers } from "redux";
+import reducerTodo from "./reducer";
+
+const combine = combineReducers({
+    reducerTodo: reducerTodo
+})
+
+export default combine
